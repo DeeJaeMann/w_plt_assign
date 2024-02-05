0bd751312cf74f39fb1c474e5e443a6f94e83043
Dee J. Mann <dee.j.mann@hotmail.com>
2024-02-05 14:44:43 -0500

    Added play_game framework

--- prompt-eng-tic-tac-toe/tic-tac-toe.py
+++ prompt-eng-tic-tac-toe/tic-tac-toe.py
@@ -1,1 +1,57 @@
 #!/usr/bin/env python3.12
+
+'''
+    Features:
+    High-Level Features:
+     - Human player turn
+     - Computer player turn
+     - Getting human player input
+     - Printing the board for the human player to see
+     - Check after every move to verify:
+        - Someone won
+        - Cats game
+     
+    Computer AI
+     - Long Term: Smart AI
+     - Short Term: functional - simple (random gen)
+
+    User Input:
+     - User inputs their move for each turn
+        - Input Row & Column for move
+        - Long Term: Allow user to specify X or O
+        - Display current board state
+
+    Flow:
+     - Who goes first -> Player A (Human)
+     - A makes move
+     - Player B turn (AI)
+     - ... until game finished
+
+    Human Player:
+     - Need to be able to see the board
+     - Prompt for move
+     - Instructions on how to enter move
+
+    AI:
+     - Random gen move
+     - Need to know if generated move is valid or not
+'''
+
+# Begin Helper Functions
+
+
+
+# End Helper Functions
+
+def play_game() :
+    """Runs the main Tic-Tac-Toe game
+    
+    Args:
+        none
+    Returns:
+        Status of whether the player won or not
+    """
+
+    pass
+
+print(play_game())
