67a84111d8cc7ee2c5e1714d18d16028a2e7892f
Dee J. Mann <dee.j.mann@hotmail.com>
2024-02-05 17:05:33 -0500

    Added More functionality.  Logic errors present

--- prompt-eng-tic-tac-toe/tic-tac-toe.py
+++ prompt-eng-tic-tac-toe/tic-tac-toe.py
@@ -3,6 +3,8 @@
 from rich.table import Table
 from rich.console import Console
 from rich import box
+import re
+import random
 
 '''
     Features:
@@ -41,6 +43,13 @@ from rich import box
      - Need to know if generated move is valid or not
 '''
 
+# Constants for positions
+X = "X"
+O = "O"
+EMPTY = ""
+
+
+
 # Begin Helper Functions
 
 def display_board(lst_board) :
@@ -57,11 +66,91 @@ def display_board(lst_board) :
     table.add_column("B", justify="center")
     table.add_column("C", justify="center")
 
-    table.add_row("1",f'{lst_board[0][0]}',f'{lst_board[1][0]}',f'{lst_board[2][0]}')
-    table.add_row("2",f'{lst_board[0][1]}',f'{lst_board[1][1]}',f'{lst_board[2][1]}')
-    table.add_row("3",f'{lst_board[0][2]}',f'{lst_board[1][2]}',f'{lst_board[2][2]}')
+    table.add_row("1",f'{lst_board[0][0]}',f'{lst_board[0][0]}',f'{lst_board[0][0]}')
+    table.add_row("2",f'{lst_board[1][0]}',f'{lst_board[1][1]}',f'{lst_board[1][2]}')
+    table.add_row("3",f'{lst_board[2][0]}',f'{lst_board[2][1]}',f'{lst_board[2][2]}')
 
     return table
+
+def get_row(str_move) :
+    """Converts the row to an index by subracting 1
+    Args:
+        str_move String with the coordinates of the player move
+    Returns:
+        Converted index for use in the board list
+    """
+    return int(str_move[1])-1
+
+def get_col(str_move) :
+    """Converts the column from a letter to an index
+    Args:
+        str_move String with the coordinates of the player move
+    Returns:
+        Converted index for use in the board list
+        """
+    # Convert the column from a letter to a position
+    match str_move[0].upper() :
+        case "A" :
+            return 0
+        case "B" :
+            return 1
+        case _:
+            return 2
+
+def validate_move(str_move, lst_board) :
+    """Validates the move entered to open board positions
+
+    Args:
+        str_move Character and Number for board position
+        lst_board Current board state (list)
+    Returns:
+        True if move is valid
+        False if position is taken
+    """
+
+    int_col = get_col(str_move)
+    int_row = get_row(str_move)
+
+    if lst_board[int_row][int_col] == EMPTY : 
+        return True
+    
+    return False
+
+def update_move(str_move, lst_board, str_turn) :
+    int_col = get_col(str_move)
+    int_row = get_row(str_move)
+
+    if str_turn == "player" :
+        lst_board[int_row][int_col] = X
+    else :
+        lst_board[int_row][int_col] = O
+
+    return lst_board
+
+def generate_ai_move() :
+    int_col = random.choice([0,1,2])
+    int_row = random.choice([0,1,2])
+
+    return f'{int_row}{int_col}'
+
+def check_rows_and_columns(lst_board) :
+
+    for int_row in lst_board:
+        if len(set(int_row)) == 1:
+            return int_row[0]
+        
+    for int_col in range(len(lst_board)) :
+        if len(set(lst_board[int_row][int_col] for int_row in range(len(lst_board)))) == 1:
+            return lst_board[0][int_col]
+        
+    return None
+
+def check_board(lst_board) :
+
+    str_rows_cols_winner = check_rows_and_columns(lst_board)
+
+    return str_rows_cols_winner
+    
 
 # End Helper Functions
 
@@ -74,14 +163,59 @@ def play_game() :
         Status of whether the player won or not
     """
 
+    # Game state
+    game_over = False
+    str_turn = "player"
+
+    console = Console()
+
     # Create a blank tic-tac-toe board
     lst_board = [
-        ['','',''],
-        ['','',''],
-        ['','',''],
+        [EMPTY,EMPTY,EMPTY],
+        [EMPTY,EMPTY,EMPTY],
+        [EMPTY,EMPTY,EMPTY],
     ]
-    console = Console()
-    console.print(display_board(lst_board))
+
+    # Main game loop
+    while game_over == False :
+        # Display the game board
+        console.print(display_board(lst_board))
+
+        if str_turn == "player" :
+            # Get user input
+            str_player_move = input("Enter your move (Letter then Number ie. A1): ")
+
+            # Validate user input
+            regex_pattern = re.compile("^[ABC][123]$", re.I)
+
+            if re.search(regex_pattern, str_player_move) :
+                # We have valid input
+                if validate_move(str_player_move, lst_board) :
+                    lst_board = update_move(str_player_move, lst_board, str_turn)
+
+                    console.print(display_board(lst_board))
+
+                    str_turn = "ai"   
+                else :
+                    print("That position is taken!")
+                pass
+            else :
+                # Invalid input
+                print("That is not a valid move!")
+        else :
+            str_ai_move = generate_ai_move()
+            while validate_move(str_ai_move, lst_board) == False :
+                str_ai_move = generate_ai_move()
+            
+            lst_board = update_move(str_ai_move, lst_board, str_turn)
+            str_turn = "player"
+        
+        #game_over = True
+        if(check_board(lst_board)) :
+            game_over = True
+
+
+    return str_player_move
 
 print(play_game())
 
