dc8212c967bbd6b0718cb1b8fbd13dd4da86bd95
Dee J. Mann <dee.j.mann@hotmail.com>
2024-02-04 21:14:35 -0500

    Added splitDigits - to separate digits into an array for processing

--- algo-armstrong-numbers/js/armstrongNumbers.js
+++ algo-armstrong-numbers/js/armstrongNumbers.js
@@ -1,6 +1,30 @@
 #!/usr/bin/env node
 // How can you make this more scalable and reusable later?
 
+
+/**
+ * This function splits the digits of a number into an array.
+ * @param {number} intNum Number to be split
+ * @returns Array of digits from intNum
+ */
+function splitDigits(intNum) {
+
+    let arrResult = [];
+
+    // Iterate through the number (as a string) and match each digit (globally)
+    for(let strDigit of `${intNum}`.matchAll(/\d/g)) {
+        arrResult.push(strDigit[0])
+    }
+    return arrResult;
+}
+
+
+/**
+ * This function accepts a range of numbers as an array and returns the Armstrong Numbers
+ * found in that array
+ * @param {array} arrInput Array of a range of numbers to search for Armstrong Numbers
+ * @returns Array containing Armstrong Numbers found in the given range
+ */
 function findArmstrongNumbers(arrInput) {
     let arrResult = [];
 
@@ -9,14 +33,11 @@ function findArmstrongNumbers(arrInput) {
         // Iterate through the characters of the string to calculate
         // the number to determine if it is an Armstrong Number or not
 
-        let intMyNum = arrInput[intIndex];
-        let arrDigits = [];
-        for(let strDigit of `${intMyNum}`.matchAll(/\d/g)) {
-            arrDigits.push(strDigit)
-        }
+        let intThisNum = arrInput[intIndex];
 
+        let arrThisNumDigits = splitDigits(intThisNum)
 
-        console.log(`Line: ${arrDigits} myNum ${intMyNum}`);
+        console.log(arrThisNumDigits)
     }
 
 
