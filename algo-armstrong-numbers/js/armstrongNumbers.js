379837531b97dff7b5aa3c89fa2cdc0f5c79e1a5
Dee J. Mann <dee.j.mann@hotmail.com>
2024-02-04 20:34:45 -0500

    Added armstrongNumber() framework

--- algo-armstrong-numbers/js/armstrongNumbers.js
+++ algo-armstrong-numbers/js/armstrongNumbers.js
@@ -1,5 +1,19 @@
+#!/usr/bin/env node
 // How can you make this more scalable and reusable later?
+
+function armstrongNumber(intNum) {
+
+    if(intNum >= 0 && intNum <= 999) {
+
+        
+        return intNum;
+    }
+
+    return "Invalid Number: Must be between 0 and 999";
+}
 
 exports.findArmstrongNumbers = function() {
 
 };
+
+console.log(armstrongNumber(5));
