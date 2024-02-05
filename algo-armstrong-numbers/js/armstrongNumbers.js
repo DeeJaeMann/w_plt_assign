6e84f9a043ce3a786261744eb91537720c2d9db0
Dee J. Mann <dee.j.mann@hotmail.com>
2024-02-04 21:04:07 -0500

    Added logic to process number into digits

--- algo-armstrong-numbers/js/armstrongNumbers.js
+++ algo-armstrong-numbers/js/armstrongNumbers.js
@@ -1,19 +1,32 @@
 #!/usr/bin/env node
 // How can you make this more scalable and reusable later?
 
-function findArmstrongNumbers(intNum) {
+function findArmstrongNumbers(arrInput) {
+    let arrResult = [];
 
-    if(intNum >= 0 && intNum <= 999) {
+    for(let intIndex in arrInput) {
+        // Take each number from the array and turn it into a string
+        // Iterate through the characters of the string to calculate
+        // the number to determine if it is an Armstrong Number or not
+
+        let intMyNum = arrInput[intIndex];
+        let arrDigits = [];
+        for(let strDigit of `${intMyNum}`.matchAll(/\d/g)) {
+            arrDigits.push(strDigit)
+        }
 
 
-        return intNum;
+        console.log(`Line: ${arrDigits} myNum ${intMyNum}`);
     }
 
-    return "Invalid Number: Must be between 0 and 999";
+
+
+    return arrResult;
+
 }
 
 exports.findArmstrongNumbers = function() {
 
 };
 
-console.log(findArmstrongNumbers(5));
+console.log(findArmstrongNumbers([546, 25, 3]));
