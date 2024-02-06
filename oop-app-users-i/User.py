b717387b45525c84640c6e23187241e5c500f63f
Dee J. Mann <dee.j.mann@hotmail.com>
2024-02-06 11:56:26 -0500

    Added class, users and print statements to User.py

--- oop-app-users-i/User.py
+++ oop-app-users-i/User.py
@@ -1,1 +1,15 @@
+#!/usr/bin/env python3.12
 # your User class goes here
+
+class User:
+    """User Class for profile information"""
+    def __init__(self, name, email_address, driver_license):
+        self.name = name
+        self.email_address = email_address
+        self.driver_license = driver_license
+
+user_steve = User("Steve", "me@here.com", "113-223-123")
+user_joe = User("Joe", "you@there.net", "345-234-532")
+
+print(f"Name: {user_steve.name} E-Mail: {user_steve.email_address} Driver License: {user_steve.driver_license}")
+print(f"Name: {user_joe.name} E-Mail: {user_joe.email_address} Driver License: {user_joe.driver_license}")
