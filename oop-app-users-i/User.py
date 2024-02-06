b9613e1959ed85bd45928cab6b987d921bfdcd81
Dee J. Mann <dee.j.mann@hotmail.com>
2024-02-06 12:30:16 -0500

    Updated output string method

--- oop-app-users-i/User.py
+++ oop-app-users-i/User.py
@@ -18,7 +18,7 @@ class User:
         self.driver_license = driver_license
 
     def __str__(self) -> str:
-        return f"Full Name: {self.first_name} {self.last_name}\n \
+        return f"*****\nFull Name: {self.first_name} {self.last_name}\n \
 DOB: {self.dob}\n \
 E-Mail: {self.email}\n \
 Driver License: {self.driver_license}\n \
