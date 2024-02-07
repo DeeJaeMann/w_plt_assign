b2fc2888576c99ba260ae5e6971e5b5b93b3a37d
Dee J. Mann <dee.j.mann@hotmail.com>
2024-02-06 22:03:41 -0500

    Added test cases and additional advance logic

--- oop-student-registry/student_registry.py
+++ oop-student-registry/student_registry.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3.12
 import re
+import sys
 
 class Student:
     """Student class for use in school registry"""
     def __init__(self, name, age = 13, grade = "12th"):
-        self._name = name
-        self._age = age
-        self._grade = grade
+        self._name = 'Invalid'
+        self._age = 'Invalid'
+        self._grade = 'Invalid'
+
+        self.set_name = name
+        self.set_age = age
+        self.set_grade = grade
 
     def __str__(self) :
         return f"Student 1: Name: {self._name}, Age: {self._age}, Grade: {self._grade}"
@@ -25,7 +30,7 @@ class Student:
         Returns:
             Student name
         """
-        return self._name.capitalize()
+        return self._name
     
     @get_name.setter
     def set_name(self, name) : 
@@ -36,12 +41,12 @@ class Student:
         Returns:
             Message only on fail
         """
-        re_pattern = "^[A-Z][a-zA-Z]{2,}$"
+        re_pattern = r"^[a-zA-Z]{3,}$"
         matches = re.match(re_pattern, f"{name}")
         if matches == None:
              print("Enter a valid name: 3 characters or more")
         else:
-            self._name = name
+            self._name = name.title()
 
     @property
     def get_age(self) :
@@ -65,7 +70,7 @@ class Student:
         """
         if type(age) == int and 11 < age < 18 :
             self._age = age
-        else: print("invalid")
+        else: print("Invalid age: outside of age range (12-17)")
         
 
     @property
@@ -94,6 +99,7 @@ class Student:
              print("Enter a valid grade as string")
         elif  8 < self.convert_grade(grade) < 13:
             self._grade = grade
+            # return True
         else: print("Outside of grade range (9-12 only)")
 
     # Methods
@@ -110,20 +116,36 @@ class Student:
         """
         grade = self.convert_grade(self.get_grade)
 
+        # Assign graduation status after 12th grade
+        if grade + 1 > 12:
+            self._grade = 'Graduated'
+            return f"{self.get_name} has Graduated 🎉"
+        # Ensure advance statement only cannot be decremented with negative values
+        elif grade + 1 < grade:
+            return 'Cannot go back a grade'
+        
         grade += years
 
         grade = f"{grade}th"
+        
         # Calls grade into grade setting and captures any invalid entry output into grade_result
-        grade_result = self.set_grade = grade
+        self.set_grade = grade
 
-        if not grade_result :
-            return f"{self.get_name} has advanced to the {self.get_grade} grade"
-
-    def study(self, subject) :
-        pass
+        return f"{self.get_name} has advanced to the {self.get_grade} grade"
 
 
-student = Student("Jaxson", 10, '7th')
+    def study(self, subject) -> str:
+        """"Displays message describing subject studying
+
+        Args:
+            subject Subject studying
+        Returns:
+            Message including subject
+        """
+
+        return f"{self.get_name} is studying {str(subject).title()}"
+
+# student = Student("Jaxson", 10, '7th')
 
 
 # print(student.get_name)
@@ -136,6 +158,9 @@ student = Student("Jaxson", 10, '7th')
 # # student.set_grade = 'not a gradeth'
 # student.set_grade = '11th'
 # print(student.get_grade)
-print(student)
-student.advance(3)
-print(student)
+# print(student)
+# student.advance(3)
+# print(student)
+# # print(student.study('underwater basket weaving'))
+# student2 = Student('Alice', grade = '11th')
+# print(student2.advance(2))
