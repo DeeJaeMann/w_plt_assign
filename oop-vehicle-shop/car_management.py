63b696ac1cc591ae56b69a518040d2c552e1918d
Dee J. Mann <dee.j.mann@hotmail.com>
2024-02-07 15:39:21 -0500

    Added convert_services

--- oop-vehicle-shop/car_management.py
+++ oop-vehicle-shop/car_management.py
@@ -14,14 +14,15 @@ class CarManager:
         self._model = str(model).lower()
         self._year = int(year)
         self._mileage = int(mileage)
-        self._services = services
+        # Ensures our property is a list
+        self._services = self.convert_services(services)
 
         CarManager.total_cars += 1
         self.add_car()
 
 
     def __str__(self) :
-        return f"ID: {self.id} Make: {self.make} Model: {self.model} Year: {self.year} Mileage: {self._mileage} Services: {self._services}"
+        return f"ID: {self.id} Make: {self.make} Model: {self.model} Year: {self.year} Mileage: {self.mileage} Services: {self.services}"
     
     def __repr__(self) :
         return str(self)
@@ -105,14 +106,14 @@ class CarManager:
         Args:
             int_year - Int to change the year into
         Returns:
-            None
+            Warning on fail
         """
         if type(int_year) == int:
             self._year = int_year
 
             self.update_cars(self._id)
         else :
-            print("Warning: Year must be an integer!  Property not updated!")
+            print("Warning: Year must be an integer! Property not updated!")
 
     @property
     def mileage(self) :
@@ -124,6 +125,33 @@ class CarManager:
             (integer) Mileage of car 
         """
         return self._mileage
+    
+    @mileage.setter
+    def mileage(self, int_mileage) :
+        """Setter: Sets the mileage property
+        
+        Args:
+            int_mileage - Integer to set mileage property to
+        Returns:
+            Warning on fail
+        """
+        if type(int_mileage) == int:
+            self._mileage = int_mileage
+
+            self.update_cars(self._id)
+        else :
+            print("Warning: Mileage must be an integer! Property not updated!")
+
+    @property
+    def services(self) :
+        """Getter: Gets the list of services from
+
+        Args:
+            None
+        Returns:
+            (list) List of services
+        """
+        return self._services
 
     # End Getters and Setters
 
@@ -150,7 +178,7 @@ class CarManager:
         """Helper Method: Updates a car in all_cars
 
         Args:
-            id - Integer of car ID
+            int_id - Integer of car ID
         Returns:
             None
         """
@@ -163,6 +191,27 @@ class CarManager:
                 "mileage" : self._mileage,
                 "services" : self._services
                 }
+        
+    def convert_services(self, lst_services) :
+        """Validates input is a list and converts if necessary
+
+        Args:
+            lst_services - Input to validate and convert
+        Returns:
+            (list) Validated and converted list
+        """
+        if lst_services == [] :
+            # No conversion necessary
+            return lst_services
+        elif type(lst_services) == list :
+            # No conversion necessary
+            return lst_services
+        else : 
+            # Create empty list
+            lst_result = []
+            # Append our input to our list
+            lst_result.append(lst_services)
+            return lst_result
 
     # End Helper Methods
 
@@ -174,21 +223,21 @@ class CarManager:
         Returns:
             (string) Menu display
         """
-        print("\n----  WELCOME  ----\n\
+        return "\n----  WELCOME  ----\n\
 1. Add a car\n\
 2. View all cars\n\
 3. View total number of cars\n\
 4. See a car's details\n\
 5. Service a car\n\
 6. Update mileage\n\
-7. Quit\n")
+7. Quit\n"
 
     def get_input(self) :
         pass
 
 
 
-car1 = CarManager("Dodge", "Charger", 1978, 50000)
+car1 = CarManager("Dodge", "Charger", 1978, 50000, "oil change")
 
 # print(car1)
 
@@ -204,17 +253,10 @@ car2 = CarManager("Chevy", "Camaro", 1969, 90000)
 
 print(CarManager.all_cars)
 
-print(car1.mileage)
-# # print(CarManager.total_cars)
+# print(car1.mileage)
 
-# car1.make = "Jeep"
+# car1.mileage = "steve"
 
-# print(CarManager.all_cars)
-
-# car1.model = "Firebird"
-
-# print(CarManager.all_cars)
-
-# car1.year = "green"
+# print(car1.mileage)
 
 # print(CarManager.all_cars)
