f712fdb10fb0ef661f4a4e5bfa6d804c172d7230
Dee J. Mann <dee.j.mann@hotmail.com>
2024-02-07 14:41:34 -0500

    Added Getters and Setters for Make and Model

--- oop-vehicle-shop/car_management.py
+++ oop-vehicle-shop/car_management.py
@@ -10,29 +10,110 @@ class CarManager:
     # Constructor
     def __init__(self, make, model, year, mileage, services = []) :
         self._id = CarManager.total_cars
-        self._make = make
-        self._model = model
+        self.make = make
+        self.model = model
         self._year = year
         self._mileage = mileage
         self._services = services
 
         CarManager.total_cars += 1
+        self.add_car()
+
+
+    def __str__(self) :
+        return f"ID: {self._id} Make: {self.make} Model: {self.model} Year: {self._year} Mileage: {self._mileage} Services: {self._services}"
+    
+    def __repr__(self) :
+        return str(self)
+
+    @property
+    def id(self) :
+        """Getter: Returns the ID of the car
+
+        Args:
+            None
+        Returns:
+            self._id - Integer of car ID
+        """
+        return self._id
+    
+    @property
+    def make(self) :
+        """Getter: Returns the make of the car
+
+        Args:
+            None
+        Returns
+            self._make - Make property of car
+        """
+        return self._make
+    
+    @make.setter
+    def make(self, str_make) :
+        """Setter: Sets the make of the car
+
+        Args:
+            make - String to change the make into
+        Returns:
+            None
+        """
+        self._make = str(str_make).lower()
+    
+    @property
+    def model(self) :
+        """Getter: Returns the model of the car
+
+        Args:
+            None
+        Returns:
+            self._model - Model property of car
+        """
+        return self._model
+    
+    @model.setter
+    def model(self, str_model) :
+        """Setter: Sets the model of the car
+
+        Args:
+            model - String to change the model into
+        Returns:
+            None
+        """
+        self._model = str(str_model).lower()
+
+    def add_car(self) :
+        """Helper Method: Adds new car to all_cars
+
+        Args:
+            None
+        Returns:
+            None
+        """
         CarManager.all_cars.append({
-                "id" : self._id,
-                "make" : self._make,
-                "model" : self._model,
+                "id" : self.id,
+                "make" : self.make,
+                "model" : self.model,
                 "year" : self._year,
                 "mileage" : self._mileage,
                 "services" : self._services
                 })
 
-    def __str__(self) :
-        return f"ID: {self._id} Make: {self._make} Model: {self._model} Year: {self._year} Mileage: {self._mileage} Services: {self._services}"
-    
-    def __repr__(self) :
-        return str(self)
+    def update_cars(self, int_id) :
+        """Helper Method: Updates a car in all_cars
 
-
+        Args:
+            id - Integer of car ID
+        Returns:
+            None
+        """
+        CarManager.all_cars[int_id] = {
+                "id" : self._id,
+                "make" : self.make,
+                "model" : self._model,
+                "year" : self._year,
+                "mileage" : self._mileage,
+                "services" : self._services
+                }
 
     def display_menu(self) :
         """Displays the menu
@@ -58,11 +139,17 @@ class CarManager:
 
 car1 = CarManager("Dodge", "Charger", 1978, 50000)
 
-print(car1)
+# print(car1)
+
+# car1.make = "Jeep"
+
+# print(car1)
 
 car2 = CarManager("Chevy", "Camaro", 1969, 90000)
 
-print(car2)
+# print(car2)
+
+
 
 print(CarManager.all_cars)
-# print(CarManager.total_cars)
+# # print(CarManager.total_cars)
