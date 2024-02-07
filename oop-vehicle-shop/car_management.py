c754d2e283780a47a63c5102bf435db27e24e93f
Dee J. Mann <dee.j.mann@hotmail.com>
2024-02-07 14:15:14 -0500

    Added ID logic and all_cars data structure

--- oop-vehicle-shop/car_management.py
+++ oop-vehicle-shop/car_management.py
@@ -8,8 +8,8 @@ class CarManager:
     total_cars = 0
     
     # Constructor
-    def __init__(self, id, make, model, year, mileage, services) :
-        self._id = id
+    def __init__(self, make, model, year, mileage, services = []) :
+        self._id = CarManager.total_cars
         self._make = make
         self._model = model
         self._year = year
@@ -17,8 +17,23 @@ class CarManager:
         self._services = services
 
         CarManager.total_cars += 1
+        CarManager.all_cars.append({
+                "id" : self._id,
+                "make" : self._make,
+                "model" : self._model,
+                "year" : self._year,
+                "mileage" : self._mileage,
+                "services" : self._services
+                })
 
-    # Methods
+    def __str__(self) :
+        return f"ID: {self._id} Make: {self._make} Model: {self._model} Year: {self._year} Mileage: {self._mileage} Services: {self._services}"
+    
+    def __repr__(self) :
+        return str(self)
+
+
+
     def display_menu(self) :
         """Displays the menu
 
@@ -41,6 +56,13 @@ class CarManager:
 
 
 
-car1 = CarManager(1, "Dodge", "Charger", 1978, 50000, "None")
+car1 = CarManager("Dodge", "Charger", 1978, 50000)
 
-car1.display_menu()
+print(car1)
+
+car2 = CarManager("Chevy", "Camaro", 1969, 90000)
+
+print(car2)
+
+print(CarManager.all_cars)
+# print(CarManager.total_cars)
