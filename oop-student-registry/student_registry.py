79dd4263306ab5dfbe6c63a05860e1dc15bcc39f
Dee J. Mann <dee.j.mann@hotmail.com>
2024-02-06 20:51:53 -0500

    Added advance method

--- oop-student-registry/student_registry.py
+++ oop-student-registry/student_registry.py
@@ -8,6 +8,14 @@ class Student:
         self._age = age
         self._grade = grade
 
+    def __str__(self) :
+        return f"Student 1: Name: {self._name}, Age: {self._age}, Grade: {self._grade}"
+
+    # Helper methods
+    def convert_grade(self, grade) :
+        return int(grade[:-2])
+
+    # Getters and Setters
     @property
     def get_name(self) :
         """Gets the name property
@@ -17,11 +25,11 @@ class Student:
         Returns:
             Student name
         """
-        return self._name
+        return self._name.capitalize()
     
     @get_name.setter
     def set_name(self, name) : 
-        """Updates students name onl if the name is 3 or more characters, holds no spaces or special characters and is in title format
+        """Updates students name only if the name is 3 or more characters, holds no spaces or special characters and is in title format
         
         Args:
             name Name to set
@@ -84,10 +92,35 @@ class Student:
         matches = re.match(re_pattern, f'{grade}')
         if matches == None:
              print("Enter a valid grade as string")
-        elif  8 < int(grade[:-2]) < 13:
+        elif  8 < self.convert_grade(grade) < 13:
             self._grade = grade
-        else: print('Outside of grade range (9-12 only)')
+        else: print("Outside of grade range (9-12 only)")
 
+    # Methods
+        
+
+    
+    def advance(self, years = 1) :
+        """Adds years to current grade if valid
+
+        Args:
+            years Number of years to add
+        Returns:
+            Success or Fail message
+        """
+        grade = self.convert_grade(self.get_grade)
+
+        grade += years
+
+        grade = f"{grade}th"
+        # Calls grade into grade setting and captures any invalid entry output into grade_result
+        grade_result = self.set_grade = grade
+
+        if not grade_result :
+            return f"{self.get_name} has advanced to the {self.get_grade} grade"
+
+    def study(self, subject) :
+        pass
 
 
 student = Student("Jaxson", 10, '7th')
@@ -103,3 +136,6 @@ student = Student("Jaxson", 10, '7th')
 # # student.set_grade = 'not a gradeth'
 # student.set_grade = '11th'
 # print(student.get_grade)
+print(student)
+student.advance(3)
+print(student)
