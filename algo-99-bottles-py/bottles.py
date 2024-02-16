3879aa209866b519cb8c0416f4649ab92ff14152
Dee J. Mann <dee.j.mann@hotmail.com>
2024-02-16 14:37:11 -0500

    Cleaned up bottles.py

--- algo-99-bottles-py/bottles.py
+++ algo-99-bottles-py/bottles.py
@@ -1,10 +1,6 @@
-#def bottle_song():
-	# write your code here!
-#	pass
+#!/usr/bin/env python3.12
 
 #bottle_song()
-
-####kldjsfalkfdajalskdfjdfls
 
 def bottle_song(num):
 	 #= num
@@ -18,26 +14,9 @@ def bottle_song(num):
 		num = num-1
 	if num ==1:
 		print(f"{num} bottle of beer on the wall, {num} bottle of beer. Take one down and pass it around, no more bottles of beer on the wall. \nNo more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.")
-# 1 bottle of beer on the wall, 1 bottle of beer. )
 
-# write your code here!
-
-#(bottle_song(99))
-
-# Exit case
-# if num == 0
-#	exit
-
-
-# if(num > 3) :
-#	print normal message
-# elif num == 2:
-#	2 msg
-# else:
-#	last message
 
 def bottle_song_two(num):
-
 
 	if num >=3:
 		main_string = f"{num} bottles of beer on the wall, {num} bottles of beer. Take one down and pass it around, {num-1} bottles of beer on the wall."
@@ -54,8 +33,6 @@ def bottle_song_two(num):
 		# num is 0
 		# exit case
 		return
-
-
 
 	if num > 0:
 		return bottle_song_two(num-1)
