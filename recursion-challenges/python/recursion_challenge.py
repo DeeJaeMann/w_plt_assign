4431b6e621b939414f365f79aaefdcc317ebfd06
Dee J. Mann <dee.j.mann@hotmail.com>
2024-02-19 14:00:40 -0500

    Added palindrome()

--- recursion-challenges/python/recursion_challenge.py
+++ recursion-challenges/python/recursion_challenge.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3.12
+import re
 
 def factorial(x):
 	
@@ -9,7 +10,23 @@ def factorial(x):
 
 def palindrome(string):
 	# A string that is the same forward as it is backward
-	pass
+
+	# Create a list from the characters matched, ignore spaces, punctuation and case
+	regex_pattern = re.compile(r'[a-z]')
+	lst = regex_pattern.findall(string.lower())
+
+	# Exit case:
+	# If we have no more elements in the array then we have found a palindrome
+	if len(lst) == 0 :
+		return True
+	
+	# Check if the first and last indexes are the same
+	if lst[0] == lst[-1] :
+		# Remove the first and last index and join the characters into a string
+		string = "".join(lst[1:-1])
+		return palindrome(string)
+	else :
+		return False
 
 def bottles(num):
 	pass
@@ -17,4 +34,6 @@ def bottles(num):
 def roman_num(num):
 	pass
 
-print(factorial(5))
+# print(factorial(5))
+print(palindrome("A man, a plan, a canal -- Panama"))
+print(palindrome("Pizza"))
