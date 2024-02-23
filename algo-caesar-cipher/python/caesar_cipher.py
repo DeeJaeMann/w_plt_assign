7e92d74969f2e625c8d1e9e4786ecc4a3c3ffb68
Dee Jae Mann <dee.j.mann@hotmail.com>
2024-02-22 22:36:57 -0500

    Fixed caesar cipher logic.  Made OOP

--- algo-caesar-cipher/python/caesar_cipher.py
+++ algo-caesar-cipher/python/caesar_cipher.py
@@ -1,8 +1,11 @@
-#!/usr/bin/env python3.12
+#!/usr/bin/env python3
+import re
 
 class Message :
-    ASCII_START = ord('A')
-    ASCII_END = ord('z')
+    UPPER_START = ord('A')
+    UPPER_END = ord('Z')
+    LOWER_START = ord('a')
+    LOWER_END = ord('z')
 
     def __init__(self, message=None) :
         self._message = message
@@ -15,34 +18,54 @@ class Message :
     def message(self, string) :
         self._message = string
 
+    def convert_letters(self, offset, char_list) :
+        result_list = []
+
+        for char in char_list :
+
+            # Check uppercase letters
+            if re.match(r'[A-Z]', char) :
+                max_number = Message.UPPER_END
+                min_number = Message.UPPER_START
+
+            # Check lowercase letters
+            elif re.match(r'[a-z]', char) :
+                max_number = Message.LOWER_END
+                min_number = Message.LOWER_START
+
+            # Char is not a letter
+            else :
+                result_list.append(char)
+                continue
+
+            # Check if the character ASCII value is greater than our highest allowed value
+            if (ord(char) + offset) > max_number and offset > 0:
+
+                # Calculate the remainder after our highest value
+                new_offset = (ord(char) + offset) - max_number
+                # Add that value to our start value 
+                new_char = chr(min_number + new_offset)
+                result_list.append(new_char)
+            
+            elif (ord(char) + offset) < min_number:
+
+                # Calculate how far before our lowest value
+                new_offset = min_number - (ord(char) + offset)
+                new_char = chr(max_number - new_offset)
+                result_list.append(new_char)
+
+            else :
+
+                new_char = chr(ord(char)+offset)
+                result_list.append(new_char)
+
+        return result_list
+
     def cipher_message(self, offset=0) :
 
-        def convert_letters(offset, char_list) :
-            result_list = []
-
-            for char in char_list :
-                if (ord(char) + offset) > Message.ASCII_END :
-                    offset = Message.ASCII_END - (ord(char) + offset)
-                    new_char = chr(Message.ASCII_START + offset - 1)
-                    result_list.append(new_char)
-
-                else :
-                    new_char = chr(ord(char)+offset)
-                    result_list.append(new_char)
-
-            return result_list
-
-        message = self._message
-
-        new_message = list(message)
-        new_message = convert_letters(offset, new_message)
+        new_message = re.findall(r'.', self.message)
+        new_message = self.convert_letters(offset, new_message)
         return "".join(new_message)
-
-# msg = Message("Hello")
-
-# cipher = msg.cipher_message(offset=5)
-
-# print(cipher)
 
 def caesar_cipher(string, shift_amount):
     msg = Message(string)
@@ -51,9 +74,7 @@ def caesar_cipher(string, shift_amount):
 
     return cipher
 
-# print(caesar_cipher("Boy! What a string!", -5))
-# print(caesar_cipher("Hello zach168! Yes here.", 5))
-# print(caesar_cipher("Hello Zach168! Yes here.", -5))
-
-for i in range(ord('A'), ord('z')) :
-    print(f"Code {i} ")
+print(caesar_cipher("Boy! What a string!", -5))
+print(caesar_cipher("Boy! What a string!", 5))
+print(caesar_cipher("Hello zach168! Yes here.", 5))
+print(caesar_cipher("Hello Zach168! Yes here.", -5))
