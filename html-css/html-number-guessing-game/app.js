61b6a23d19b4759325980ca88944df78fea2f7ad
Dee Jae Mann <dee.j.mann@hotmail.com>
2024-02-26 14:12:06 -0500

    Added guessing game logic

--- html-css/html-number-guessing-game/app.js
+++ html-css/html-number-guessing-game/app.js
@@ -1,2 +1,27 @@
-console.log("HELLO PAPA PLATOON!")
+// console.log("HELLO PAPA PLATOON!")
 // Your function(s) should go here that will interact with the webpage or DOM
+
+let intAnswer = Math.floor(Math.random()*100)
+
+// answerTag = document.getElementById("answer")
+// answerTag.textContent = intAnswer
+
+
+
+const submitGuess = (event) => {
+    event.preventDefault();
+    let frmData = new FormData(event.target)
+    let objData = Object.fromEntries(frmData)
+
+    resultTag = document.getElementById("result")
+    myGuess = Number(objData['my_guess'])
+
+    // resultTag.textContent = objData['my_guess']
+    if(myGuess === intAnswer) {
+        resultTag.textContent = "Correct!"
+    } else if(myGuess > intAnswer) {
+        resultTag.textContent = "High!"
+    } else {
+        resultTag.textContent = "Low!"
+    }
+}
