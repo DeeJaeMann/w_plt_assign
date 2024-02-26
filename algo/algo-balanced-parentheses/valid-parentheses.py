4ed71298fba2bc6a1a3dacd38f428f419d6efea2
Dee Jae Mann <dee.j.mann@hotmail.com>
2024-02-26 07:57:59 -0500

    Adjusted logic

--- algo/algo-balanced-parentheses/valid-parentheses.py
+++ algo/algo-balanced-parentheses/valid-parentheses.py
@@ -16,20 +16,35 @@ def is_valid(str_input) :
     lst_opens = re.findall(r"\(|\[|\{", str_input)
     lst_closes = re.findall(r"\)|\]|\}", str_input)
 
+    lst_opens = sorted(lst_opens)
+    lst_closes = sorted(lst_closes)
+
     # check if the list is an even number
     if (len(lst_opens) + len(lst_closes)) % 2 != 0 :
         # it isn't, we don't have balanced sets
         return False
     elif len(lst_opens) != len(lst_closes) :
         return False
+    else :
+        # compare the matches
+        for int_index, char in enumerate(lst_opens) :
+            match char:
+                case '(':
+                    if lst_closes[int_index] != ')' :
+                        return False
+
+                case '[':
+                    if lst_closes[int_index] != ']' :
+                        return False
+
+                case '{':
+                    if lst_closes[int_index] != '}' :
+                        return False
 
     return True
-
-    # print(lst_matches)
-
 
 
 print(is_valid("()"))
 print(is_valid("())"))
 print(is_valid("()()"))
-print(is_valid("(["))
+print(is_valid("({()()[]})"))
