16a2101f637238c4e1ae89db93db317b333960d7
Dee Jae Mann <dee.j.mann@hotmail.com>
2024-02-26 16:05:39 -0500

    Added comments

--- html-css/html-dom-manipulation/app.js
+++ html-css/html-dom-manipulation/app.js
@@ -1,17 +1,20 @@
 
 const replaceImages = () => {
+    // Get an array of the images from the document
     let images = document.body.getElementsByTagName('img')
 
+    // Iterate through the images array
     for(let intI = images.length - 1; intI >= 0; intI--) {
+
         let thisImage = images[intI]
  
-
+        // Check if this image has an alt
         if(thisImage.alt) {
-
+            // Create a text node from the image alt attribute
             let thisAlt = document.createTextNode(thisImage.alt)
+            // Replace the img tag with the text node
             thisImage.parentNode.replaceChild(thisAlt, thisImage)
         }
     }
    
-
 }
