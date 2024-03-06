f948a108fdb5fddd0d5871c690ce8e01b2adf367
Dee Jae Mann <dee.j.mann@hotmail.com>
2024-03-06 15:09:35 -0500

    Added Cypress Test Suite

---
+++ react/Rick-and-Morty-II/front-end/cypress.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+    e2e: {
+        baseUrl: "http://localhost:5173/",
+        supportFile: false,
+    },
+
+    component: {
+        devServer: {
+            framework: 'react',
+            bundler: 'vite'
+        },
+        supportFile: false,
+        specPattern: "./cypress/component/**/*.cy.{js,jsx,ts,tsx}",
+    },
+    viewportWidth: 1024,
+    viewportheight: 768,
+    video: true,
+    videosFolder: './cypress/videos/'
+})
