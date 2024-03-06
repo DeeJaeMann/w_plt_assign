c6c038549f35633e505e816126de5ee4f3699102
Dee Jae Mann <dee.j.mann@hotmail.com>
2024-03-06 14:38:01 -0500

    Migrated to Rick and Morty II

---
+++ react/Rick-and-Morty-II/front-end/src/pages/HomePage.jsx
@@ -0,0 +1,16 @@
+
+function HomePage() {
+    return (
+        <>
+            <div className="container-fluid">
+                <div className="jumbotron text-center">
+                    <h1>Rick and Morty</h1>
+                    {/* <p>Come watch the show with us!</p> */}
+                </div>
+                <p>Come watch the show with us!</p>
+            </div>
+        </>
+    );
+}
+
+export default HomePage;
