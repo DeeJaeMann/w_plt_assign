5c9f6b36c728da3d4ca19a5d69842cd21627e5b9
Dee Jae Mann <dee.j.mann@hotmail.com>
2024-03-12 14:51:16 -0400

    Completed schema validation and data import

--- sql/game-store-III/store/load_data_store.sql
+++ sql/game-store-III/store/load_data_store.sql
@@ -3,3 +3,8 @@
 \COPY action_figure FROM './data/action_figure.csv' WITH CSV HEADER;
 \COPY employee FROM './data/employee.csv' WITH CSV HEADER;
 \COPY poster FROM './data/poster.csv' WITH CSV HEADER;
+
+SELECT setval('game_id_seq', (SELECT MAX(id) FROM game));
+SELECT setval('action_figure_id_seq', (SELECT MAX(id) FROM action_figure));
+SELECT setval('employee_id_seq', (SELECT MAX(id) FROM employee));
+SELECT setval('poster_id_seq', (SELECT MAX(id) FROM poster));
--- sql/game-store-III/store/schema_store.sql
+++ sql/game-store-III/store/schema_store.sql
@@ -6,11 +6,11 @@ DROP TABLE IF EXISTS poster;
 
 -- Create Tables
 CREATE TABLE game (
-    game_id SERIAL PRIMARY KEY,
-    game_title VARCHAR(255) 
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(50) 
         UNIQUE 
         NOT NULL 
-        CHECK (game_title ~ '^[A-Z0-9][\w \-:''\\]+$'),
+        CHECK (title ~ '^[A-Z0-9][\w \-:''\\]+$'),
     quantity INT 
         NOT NULL 
         CHECK(quantity > 0 AND quantity < 51),
@@ -21,10 +21,10 @@ CREATE TABLE game (
 
 CREATE TABLE action_figure (
     id SERIAL PRIMARY KEY,
-    action_figure_title VARCHAR(255) 
+    title VARCHAR(50) 
         UNIQUE 
         NOT NULL 
-        CHECK(action_figure_title ~ '^[A-Z0-9][\w :''\-\\]'),
+        CHECK(title ~ '^[A-Z0-9][\w :''\-\\]'),
     quantity INT 
         NOT NULL 
         CHECK(quantity > 0 AND quantity < 31),
@@ -35,24 +35,31 @@ CREATE TABLE action_figure (
 
 CREATE TABLE employee (
     id SERIAL PRIMARY KEY,
-    employee_name VARCHAR(255) 
+    name VARCHAR(50) 
         NOT NULL 
-        CHECK(employee_name ~ '^([A-Z][A-Za-z]*) ([A-Z][A-Za-z]*\s?){1,2}$'),
-    position VARCHAR(255) 
+        CHECK(name ~ '^([A-Z][A-Za-z]*) ([A-Z][A-Za-z]*\s?){1,2}$'),
+    position VARCHAR(50) 
         NOT NULL 
         CHECK(
             position ~ '^Sales Associate|Store Manager|Inventory Clerk|Customer Service Representative|IT Specialist|Marketing Coordinator|Assistant Manager|Finance Analyst|Security Officer|HR Coordinator$'), -- match the words from the description
     salary DECIMAL(7,2)
         NOT NULL
-        CHECK(salary > 34652.80 AND salary < 65000.00)
-    -- 16.66 ~ 34652.80
+        CHECK(salary > 31999.99 AND salary < 65000.01)
+    -- 16.66 ~ 34652.80  one record set to 32000
     -- 31.25 ~ 65000.00
 );
 
 CREATE TABLE poster (
-    id INT PRIMARY KEY,
-    poster_title VARCHAR,
-    quantity INT,
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(50)
+        NOT NULL
+        UNIQUE
+        CHECK(title ~ '^[A-Z0-9][\w\-\s]*$'),
+    quantity INT
+        NOT NULL
+        CHECK(quantity > 0 AND quantity < 31),
     price DECIMAL(4,2)
+        NOT NULL
+        CHECK(price > 5.99 AND price < 20.01)
 );
 
