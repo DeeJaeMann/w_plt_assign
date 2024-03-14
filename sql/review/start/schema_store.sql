01cd0d90d90705e30790e763f65479b8d0fcd75b
Dee Jae Mann <dee.j.mann@hotmail.com>
2024-03-14 11:34:23 -0400

    Added schema_store with drop and create table logic

---
+++ sql/review/start/schema_store.sql
@@ -0,0 +1,53 @@
+-- Drop tables if they exist
+DROP TABLE IF EXISTS action_figure;
+DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS game;
+DROP TABLE IF EXISTS poster;
+
+-- Create tables
+CREATE TABLE action_figure (
+    id SERIAL 
+        PRIMARY KEY,
+    action_figure_title VARCHAR(30) 
+        NOT NULL
+        UNIQUE,
+    quantity INT
+        NOT NULL
+    price DECIMAL(4,2)
+        NOT NULL
+);
+
+CREATE TABLE employee (
+    id SERIAL 
+        PRIMARY KEY,
+    employee_name VARCHAR(30)
+        NOT NULL,
+    position VARCHAR(30)
+        NOT NULL,
+    salary DECIMAL(7,2)
+        NOT NULL
+);
+
+CREATE TABLE game (
+    game_id SERIAL
+        PRIMARY KEY,
+    game_title VARCHAR(30)
+        NOT NULL
+        UNIQUE,
+    quantity INT
+        NOT NULL,
+    price DECIMAL(4,2)
+        NOT NULL
+);
+
+CREATE TABLE poster (
+    id SERIAL
+        PRIMARY KEY,
+    poster_title VARCHAR(30)
+        NOT NULL
+        UNIQUE,
+    quantity INT
+        NOT NULL,
+    price DECIMAL(4,2)
+        NOT NULL
+);
