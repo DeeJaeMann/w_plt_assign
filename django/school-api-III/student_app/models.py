a7bec76e4286ab2d96adf420a9cb25efc601200a
Dee Jae Mann <dee.j.mann@hotmail.com>
2024-03-20 13:20:26 -0400

    Added school api iii

---
+++ django/school-api-III/student_app/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=50)
+    student_email = models.EmailField(max_length=50, unique=True)
+    personal_email = models.EmailField(max_length=50, blank=True, unique=True)
+    locker_number = models.IntegerField(unique=True, default=110)
+    locker_combination = models.CharField(max_length=10, default="12-12-12")
+    good_student = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.student_email} - {self.locker_number}"
+    
+    def locker_assignement(self, int_locker_number):
+        self.locker_number = int_locker_number
+        self.save()
+
+    def student_status(self, bool_status):
+        self.good_student = bool_status
+        self.save()
