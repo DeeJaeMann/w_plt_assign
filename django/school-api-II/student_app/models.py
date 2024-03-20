990c241ce68c50c908a52bd51c12c44c4321d80a
Dee Jae Mann <dee.j.mann@hotmail.com>
2024-03-20 08:58:38 -0400

    Completed assignment

--- django/school-api-II/student_app/models.py
+++ django/school-api-II/student_app/models.py
@@ -3,8 +3,19 @@ from django.db import models
 # Create your models here.
 class Student(models.Model):
     name = models.CharField(max_length=50)
-    student_email = models.EmailField()
-    personal_email = models.EmailField(blank=True)
-    locker_number = models.IntegerField()
-    locker_combination = models.CharField(max_length=10)
+    student_email = models.EmailField(unique=True)
+    personal_email = models.EmailField(blank=True, unique=True)
+    locker_number = models.IntegerField(unique=True, default=110)
+    locker_combination = models.CharField(max_length=10, default="12-12-12")
     good_student = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.student_email} - {self.locker_number}"
+    
+    def locker_reassignment(self, int_number):
+        self.locker_number = int_number
+        self.save()
+
+    def student_status(self, bool_status):
+        self.good_student = bool_status
+        self.save()
