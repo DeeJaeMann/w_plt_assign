015b6f8bb0d19a46ac32b4d678c936a7e5681b3e
Dee Jae Mann <dee.j.mann@hotmail.com>
2024-03-25 16:10:20 -0400

    Created base all_students view

--- django/school-api-VI/school_proj/student_app/serializers.py
+++ django/school-api-VI/school_proj/student_app/serializers.py
@@ -1,14 +1,21 @@
-from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 from .models import Student
 
 
-class StudentSerializer(serializers.ModelSerializer):
+class StudentSerializer(ModelSerializer):
     class Meta:
         model = Student
         fields = ["name", "student_email", "locker_number"]
 
 
-class StudentAllSerializer(serializers.ModelSerializer):
+class StudentAllSerializer(ModelSerializer):
+    subjects = SerializerMethodField()
+
     class Meta:
         model = Student
         exclude = ['id']
+
+    def get_subjects(self, instance):
+        subjects = instance.subjects.all()
+        ser_subjects = [{'subject_name':sub.subject_name, 'professor':sub.professor} for sub in subjects]
+        return ser_subjects
--- django/school-api-VI/school_proj/student_app/views.py
+++ django/school-api-VI/school_proj/student_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .serializers import StudentAllSerializer, Student
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+class All_students(APIView):
+    
+    def get(self, request):
+        students = Student.objects.all()
+        ser_students = StudentAllSerializer(students, many=True)
+        print(ser_students)
+        return Response(ser_students.data)
